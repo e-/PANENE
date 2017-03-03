089628a5730e8a488141ffffdd6ae872f623e419
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2017-03-03 13:33:42 +0900

    update old knn selectively using a priority queue

--- paknn/knn_table.h
+++ paknn/knn_table.h
@@ -4,6 +4,10 @@
 #include <flann/flann.hpp>
 #include <vector>
 #include <iostream>
+#include <queue>
+
+#define MAX_CHECK 0
+#define DEBUG 0
 
 template <class Indexer>
 class KNNTable {
@@ -13,13 +17,23 @@ class KNNTable {
   
 public:
   struct Neighbor {
-    ElementType index;
+    IDType id;    
     DistanceType distance;
 
-    Neighbor(ElementType index_, DistanceType distance_) : index(index_), distance(distance_) {}
+    Neighbor() = default;
+    Neighbor(IDType id_, DistanceType distance_) : id(id_), distance(distance_) {}
+
     friend std::ostream& operator<<( std::ostream& os, const Neighbor& obj ) {
-      os << "(" << obj.index << ", " << obj.distance << ")";
+      os << "(" << obj.id << ", " << obj.distance << ")";
       return os;  
+    }
+
+    bool operator< (const Neighbor &n) const {
+      return this->distance < n.distance;
+    }
+
+    bool operator> (const Neighbor &n) const {
+      return this->distance > n.distance;
     }
   };
 
@@ -28,26 +42,53 @@ public:
     indexer = Indexer(initData, indexParams_);
     searchParams = searchParams_;
   }
-  
-  void addPoints(flann::Matrix<DistanceType> points) { 
+
+  void extendDataset(const flann::Matrix<DistanceType>& new_points) { 
+    size_t old_size = points.size();
+		size_t new_size = points.size() + new_points.rows;
+
+    points.resize(new_size);
+
+    for (size_t i = old_size; i < new_size; ++i) {
+      points[i] = new_points[i - old_size];
+    }
+  }
+
+  void addPoints(const flann::Matrix<DistanceType>& new_points) { 
+    // 0. add new points to data
+
+    extendDataset(new_points);
+
     // 1. index new points
     
-    indexer.addPoints(points);
-    std::cout << indexer.veclen() << ' ' << indexer.size() << std::endl;
+    size_t old_size = indexer.size();
 
-    assert(indexer.size() > k);
+    indexer.addPoints(new_points);
+    std::cout << new_points.rows << " new points have been indexed. " << indexer.size() << " points exist in trees." << std::endl;
+
+    assert(indexer.size() > k); // check if at least k points are in the index
+
+    size_t size = indexer.size();
 
     // 2. compute new knn
   
-    flann::Matrix<IDType> indices(new IDType[points.rows * k], points.rows, k);
-    flann::Matrix<DistanceType> dists(new DistanceType[points.rows * k], points.rows, k);
-                 
-    indexer.knnSearch(points, indices, dists, k, searchParams);
+    flann::Matrix<IDType> indices(new IDType[new_points.rows * k], new_points.rows, k);
+    flann::Matrix<DistanceType> dists(new DistanceType[new_points.rows * k], new_points.rows, k);
+    std::priority_queue<Neighbor, std::vector<Neighbor>, std::greater<Neighbor>> queue; // descending order
+    flann::DynamicBitset checked(size);
 
-    for(unsigned int i = 0; i < points.rows; ++i) {
-      std::vector<Neighbor> nns;
+    indexer.knnSearch(new_points, indices, dists, k, searchParams);
+
+    std::vector<Neighbor> nns;
+    nns.resize(k);
+
+    for(unsigned int i = 0; i < new_points.rows; ++i) {
+      IDType id = old_size + i;
+      checked.set(id);
+
       for(unsigned int j = 0; j < k; ++j) {
-        nns.push_back(Neighbor(indices[i][j], dists[i][j]));
+        nns[j] = Neighbor(indices[i][j], dists[i][j]);
+        queue.push(nns[j]);
       }
       
       neighbors.push_back(nns);
@@ -56,18 +97,81 @@ public:
     delete[] indices.ptr();
     delete[] dists.ptr();
 
+#if DEBUG
     for(unsigned int i = 0; i < neighbors.size(); ++i) {
       for(unsigned int j = 0; j < k; ++j) {
         std::cout << neighbors[i][j] << ' ';
       }
       std::cout << std::endl;
     }
+#endif
+
     // 3. update old knn
 
+    unsigned int checkCount = 0;
+    flann::Matrix<IDType> new_indices(new IDType[k], 1, k);
+    flann::Matrix<DistanceType> new_dists(new DistanceType[k], 1, k);
+
+    while(!queue.empty()) {
+      auto q = queue.top();
+      queue.pop();
+
+      if(checked.test(q.id) || (MAX_CHECK > 0 && checkCount > MAX_CHECK)) continue;
+      checked.set(q.id);
+      checkCount++;
+
+      // we need to update the NN of q.id
+      
+      // get new NNs
+      
+      flann::Matrix<ElementType> qvec(points[q.id], 1, d);
+      indexer.knnSearch(qvec, new_indices, new_dists, k, searchParams);
+      
+      // check if there is a difference between previous NNs and newly computed NNs.
+      
+      unsigned int i;
+      for(i = 0; i < k; ++i) {
+        if(neighbors[q.id][i].id != new_indices[0][i])
+          break;
+      } 
+
+      if(i < k) { // if there is a difference
+        // update q.id
+        for(i = 0; i < k; ++i) {
+          Neighbor ne(new_indices[0][i], new_dists[0][i]);
+          neighbors[q.id][i] = ne;
+          if(!checked.test(ne.id)) {
+            queue.push(ne);
+          }
+        }
+      }
+
+#if DEBUG  
+      std::cout << q << std::endl;
+      for(auto &x : neighbors[q.id]) {
+        std::cout << x << ' ';
+      }
+      std::cout << std::endl;
+
+      for(unsigned int i = 0; i < k; ++i) {
+        std::cout << '(' << new_indices[0][i] << ", " << new_dists[0][i] << ") ";
+      }
+      std::cout << std::endl;
+#endif
+    }
+
+    delete[] new_indices.ptr();
+    delete[] new_dists.ptr();
+
+    std::cout << checkCount << " points have been updated." << std::endl;
   };
 
+  std::vector<Neighbor>& getNeighbors(const IDType id) {
+    return neighbors[id];
+  }
+
 private:
-  flann::Matrix<DistanceType> data;
+  std::vector<ElementType*> points;
   unsigned int d;
   unsigned int k;
   Indexer indexer;
--- paknn/paknn.cpp
+++ paknn/paknn.cpp
@@ -1,27 +1,43 @@
 #include "paknn.h"
 #include <memory>
+#include <fstream>
 
 using namespace flann;
 using namespace std;
 
+void readData(const string &path, int rows, int cols, float *dataset) {
+  std::ifstream ifs(path);
+
+  for(int i = 0; i < rows * cols; ++i) {
+    ifs >> dataset[i]; 
+  }
+}
+
 int main(){
-  int k = 5;
-  int d = 5;
+  int n = 5000; // rows per each chunk
+  int d = 100;
+  int repeat = 10;
+  int k = 10;
 
-  vector<float> values;
-  
-  for(int i = 0; i < 50; ++i) 
-    values.push_back((float)i / 50);
-
-  Matrix<float> data(&values[0], 50, d);
+  float *data = new float[n * d * repeat];
+  readData("../data/glove.shuffled.txt", n * repeat, d, data);
   SearchParams searchParam(512);
 
-  KNNTable<Index<L2<float>>> table(k, d, KDTreeIndexParams(1), searchParam);
+  KNNTable<Index<L2<float>>> table(k + 1, d, KDTreeIndexParams(4), searchParam);
 
-  for(int r = 0; r < 5; ++r) { 
-    table.addPoints(data);
+  for(int r = 0; r < repeat; ++r) { 
+    Matrix<float> dataMatrix(data + r * n, n, d);
+    table.addPoints(dataMatrix);
   }
 
+  // for testing
+  
+  int id = 5;
+  for(auto& neighbor : table.getNeighbors(id)) { 
+    cout << neighbor.id << ' ' << neighbor.distance << endl;
+  }
+
+  delete[] data;
 
   return 0;
 }
