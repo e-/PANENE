f46b706ecdd8d83dd3dbba2ac050128c656ad728
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2017-06-26 21:53:46 +0900

    added density example

--- examples/density/baseline.py
+++ examples/density/baseline.py
@@ -21,6 +21,9 @@ def main():
             ys.append(float(row[dim2]))
         
         count, xedges, yedges = np.histogram2d(xs, ys, bins=bins)
+
+    print(xedges)
+    print(yedges)
             
     with open('baseline.csv', 'w') as outfile:
         print('x,y,count', file = outfile)
@@ -33,6 +36,11 @@ def main():
     with open('data.txt', 'w') as outfile:
         for i in range(len(xs)):
             print('{} {}'.format(xs[i], ys[i]), file=outfile)
+
+    with open('data.sorted.txt', 'w') as outfile:
+        s = sorted(list(zip(xs, ys)), key=lambda x: x[0] + x[1])
+        for i in range(len(xs)):
+            print('{} {}'.format(s[i][0], s[i][1]), file=outfile)
 
 if __name__ == '__main__':
     main()
