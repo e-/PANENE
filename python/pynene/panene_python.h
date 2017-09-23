b25d1057c239083fffde70c518540c09a9ff0ad5
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2017-09-23 17:02:04 +0200

    improve

--- python/pynene/panene_python.h
+++ python/pynene/panene_python.h
@@ -13,8 +13,8 @@ class PyDataSource
   typedef float ElementType;
   typedef float DistanceType;
 
-  PyDataSource() {
-    object = Py_None;
+  PyDataSource()
+    : _d(0), _object(Py_None) {
   }
 
   ~PyDataSource() {
@@ -22,21 +22,22 @@ class PyDataSource
   }
 
   void set_array(PyObject * o) {
-    if (o == object) return;
-    Py_XINCREF(o);
-    Py_XDECREF(object);
-    object = o;
+    if (o == _object) return;
+    Py_INCREF(o);
+    Py_DECREF(_object);
+    _object = o;
+    set_dim();
   }
 
   PyObject * get_array() const {
-    return object;
+    return _object;
   }
 
   ElementType get(const IDType &id, const IDType &dim) const {
     PyObject *key1 = PyInt_FromLong(id);
     PyObject *key2 = PyInt_FromLong(dim);
     PyObject *tuple = PyTuple_Pack(2, key1, key2);
-    PyObject *pf = PyObject_GetItem(object, tuple);
+    PyObject *pf = PyObject_GetItem(_object, tuple);
     ElementType ret = (ElementType)PyFloat_AsDouble(pf);
     Py_DECREF(tuple);
     Py_DECREF(pf);
@@ -44,21 +45,23 @@ class PyDataSource
   }
 
   IDType findDimWithMaxSpan(const IDType &id1, const IDType &id2) {
-    size_t dim = 0;
+    size_t dimension = 0;
     ElementType maxSpan = 0;
+    size_t d = dim();
 
     for(size_t i = 0; i < d; ++i) {
       ElementType span = std::abs(this->get(id1, i) - this->get(id2, i));
       if(maxSpan < span) {
         maxSpan = span;
-        dim = i;
+        dimension = i;
       }
     }
 
-    return dim;
+    return dimension;
   }
 
   void computeMeanAndVar(const IDType *ids, int count, std::vector<DistanceType> &mean, std::vector<DistanceType> &var) {
+    size_t d = dim();
     mean.resize(d);
     var.resize(d);
 
@@ -92,6 +95,7 @@ class PyDataSource
 
   DistanceType distL2Squared(const IDType &id1, const IDType &id2) const {
     DistanceType sum = 0;
+    size_t d = dim();
 
     for(size_t i = 0; i < d; ++i) {
       ElementType v1 = this->get(id1, i), v2 = this->get(id2, i);
@@ -102,22 +106,38 @@ class PyDataSource
   }
 
   size_t size() const {
-    return n;
+    if (_object==Py_None) return 0;
+    return PyObject_Length(_object);
   }
 
   size_t loaded() const {
-    return n;
+    return size();
   }
 
   size_t dim() const {
-    return d;
+    return _d;
   }
 
-  size_t n;
-  size_t d;
-  bool opened = false;
  protected:  
-  PyObject * object;
+  int _d;
+  PyObject * _object;
+
+  void set_dim() {
+    if (_object==Py_None) {
+      _d = 0;
+    }
+    else {
+      PyObject * shape = PyObject_CallMethod(_object, "shape", NULL);
+      PyObject * dim = PyTuple_GetItem(shape, 1);
+      if (!PyLong_Check(dim))
+        _d = 0;
+      else {
+        _d = (int)PyLong_AsLong(dim);
+      }
+      Py_DECREF(shape);
+    }
+  }
+
 };
 
 typedef Neighbor<size_t, float> PyNeighbor;
