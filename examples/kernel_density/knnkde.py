8c7a0aa7f66d9fe692cfdb573acd91afae81877b
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2017-11-02 22:22:10 +0100

    simplify

--- examples/kernel_density/knnkde.py
+++ examples/kernel_density/knnkde.py
@@ -16,16 +16,10 @@ class KNNKernelDensity():
 
     def score_samples(self, X, k=10, bandwidth=0.2):
         _, dists = self.index.knn_search_points(X, k=k)
-
-        n = len(X)
-        scores = np.zeros(n)
-
-        for i in range(n):
-            scores[i] = self._gaussian_score(dists[i], bandwidth) / k
-
+        scores = self._gaussian_score(dists, bandwidth) / k
         return scores
 
     def _gaussian_score(self, dists, bandwidth):
         logg = -0.5 * (dists / bandwidth) ** 2
         g = np.exp(logg) / bandwidth / self.SQRT2PI
-        return g.sum()
+        return g.sum(axis=1)
