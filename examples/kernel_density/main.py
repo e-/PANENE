62263d38e76e288c279a90964bb243aa10852699
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2017-11-02 15:17:56 +0900

    KDE with multimodal distributions

--- examples/kernel_density/main.py
+++ examples/kernel_density/main.py
@@ -2,18 +2,25 @@ from sklearn.neighbors.kde import KernelDensity
 from knnkde import KNNKernelDensity
 
 import numpy as np
-N = 500
+N = 200
 X = np.random.random((N, 2)).astype(np.float32)
 
-X1 = np.array([np.random.normal([0.1, 0.3], [0.1, 0.3]) for x in range(N)]).astype(np.float32)
+def mv(n, mean, cov):
+    return np.random.multivariate_normal(mean, cov, size=(n)).astype(np.float32)
 
+X1 = np.array([np.random.normal([0.1, 0.3], [0.1, 0.3]) for x in range(N)]).astype(np.float32)
 X2 = np.array([np.random.normal([0.7, 0.5], [0.2, 0.1]) for x in range(N)]).astype(np.float32)
 
-X = np.concatenate((X1, X2), axis=0)
+X = np.concatenate((
+    mv(N, [0.1, 0.3], [[0.01, 0], [0, 0.09]]),
+    mv(N, [0.7, 0.5], [[0.04, 0], [0, 0.01]]),
+    mv(N, [-0.4, -0.3], [[0.09, 0.04], [0.04, 0.02]])
+    ), axis=0)
 
 sampleN = 30
 samples = np.indices((sampleN + 1, sampleN + 1)).reshape(2, -1).T / sampleN * 3 - 1.5
-kde = KNNKernelDensity(X, bandwidth = 0.2)
+
+kde = KNNKernelDensity(X, bandwidth = 0.2, k=10)
 scores = kde.score_samples(samples.astype(np.float32))
 
 import json
